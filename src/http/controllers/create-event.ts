49a14d1007cfe3444b84fbe445d1087b3bab7766
gustavosalviato <gustavosalviato8@gmail.com>
2024-04-04 19:49:03 -0300

    chore: add type provider zod

--- src/http/controllers/create-event.ts
+++ src/http/controllers/create-event.ts
@@ -1,43 +1,59 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { prisma } from "@/lib/prisma";
 import { generateSlug } from "@/utils/generate-slug";
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 
 export async function createEvent(app: FastifyInstance) {
-  app.post("/event", async (request, reply) => {
-    const createEventSchema = z.object({
-      title: z.string().min(4),
-      details: z.string().nullable(),
-      maximumAttendees: z.number().int().positive().nullable(),
-    });
-
-    const { title, details, maximumAttendees } = createEventSchema.parse(
-      request.body
-    );
-
-    const slug = generateSlug(title);
-
-    const eventWithSameSlug = await prisma.event.findUnique({
-      where: {
-        slug,
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/event",
+    {
+      schema: {
+        body: z.object({
+          title: z.string().min(4),
+          details: z.string().nullable(),
+          maximumAttendees: z.number().int().positive().nullable(),
+        }),
+        response: {
+          201: z.object({
+            eventId: z.string().uuid(),
+          }),
+        },
       },
-    });
+    },
+    async (request, reply) => {
+      const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+      });
 
-    if (eventWithSameSlug) {
-      throw new Error("Another event with same title already exists.");
+      const { title, details, maximumAttendees } = request.body;
+
+      const slug = generateSlug(title);
+
+      const eventWithSameSlug = await prisma.event.findUnique({
+        where: {
+          slug,
+        },
+      });
+
+      if (eventWithSameSlug) {
+        throw new Error("Another event with same title already exists.");
+      }
+
+      const event = await prisma.event.create({
+        data: {
+          title,
+          details,
+          maximumAttendees,
+          slug,
+        },
+      });
+
+      return reply.status(201).send({
+        eventId: event.id,
+      });
     }
-
-    const event = await prisma.event.create({
-      data: {
-        title,
-        details,
-        maximumAttendees,
-        slug,
-      },
-    });
-
-    return reply.status(201).send({
-      eventId: event.id,
-    });
-  });
+  );
 }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import fastify from "fastify";
 
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
 import { createEvent } from "@/http/controllers/create-event";
 
-export const app = fastify();
+export const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
 
